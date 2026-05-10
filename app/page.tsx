fe7d57ca9b814f0844ab9f54289e39efce8aa1d5
Mahdi Khadimi <mahdikhadimi14@gmail.com>
2026-05-10 11:19:20 +0430

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white text-black">
+      <div className="text-center text-4xl font-semibold">FA_AI</div>
+    </div>
+  );
+}
